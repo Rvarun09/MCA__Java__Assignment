4c2ebdab0fd45e47ce923356b7d74fdee47d239b
Varun Rauniyar <162958923+Rvarun09@users.noreply.github.com>
2025-09-08 09:34:29 +0530

    Update MemoryAllocation.java

--- MemoryAllocation.java
+++ MemoryAllocation.java
@@ -22,19 +22,19 @@ public class MemoryAllocation {
         System.out.println("\nAfter changing 'a' to 20:");
         System.out.println("a = " + a);
         System.out.println("b = " + b); 
-        System.out.println("----------------------------------------------\n");
 
         System.out.println("--- Reference Variables (references on stack, objects on heap) ---");
 
-        Person p1 = new Person("Alice");
+        Person p1 = new Person("varun");
         System.out.println("p1.name = " + p1.name);
 
         Person p2 = p1;
         System.out.println("p2.name = " + p2.name);
 
-        p2.name = "Bob";
+        p2.name = "akshay";
         System.out.println("\nAfter changing p2.name to Bob:");
         System.out.println("p2.name = " + p2.name);
         System.out.println("p1.name = " + p1.name); 
     }
 }
+
